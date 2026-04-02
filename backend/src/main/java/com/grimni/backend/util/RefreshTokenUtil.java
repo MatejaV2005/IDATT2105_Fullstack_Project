e3e86990b711d70eaa5583ff9a792a91c04eb476
MatejaV2005 <matejanorway@gmail.com>
2026-04-02 12:57:14 +0200

    add .path() and .sameSite() flags for refreshTokenCookie

--- backend/src/main/java/com/grimni/backend/util/RefreshTokenUtil.java
+++ backend/src/main/java/com/grimni/backend/util/RefreshTokenUtil.java
@@ -2,6 +2,9 @@ package com.grimni.backend.util;
 
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.time.Duration;
 import java.util.Base64;
@@ -16,11 +19,21 @@ public class RefreshTokenUtil {
         return Base64.getUrlEncoder().withoutPadding().encodeToString(bytearr); // converts it into string of readable ASCII values
     }
 
-    public void setHttpOnly(String tokenValue) {
+    public ResponseCookie createRefreshTokenCookie(String tokenValue) {
         ResponseCookie cookie = ResponseCookie.from("refresh_token", tokenValue)
                                 .httpOnly(true) // protects from XSS, making it httpOnly
                                 .secure(true) // can only be sent over HTTPS
                                 .maxAge(Duration.ofDays(7)) // expires after 7 days
+                                .path("/") // scopes the cookie to be sent on all endpoints
+                                .sameSite("Strict") // protects from CSRF
                                 .build();
+
+        return cookie;
+    }
+
+    public String hashToken(String token) throws NoSuchAlgorithmException {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256"); // returns a message digest object that implements the specified hash algo
+        byte[] hashBytes = digest.digest(token.getBytes(StandardCharsets.UTF_8)); // transforms the token into its correspondign array of bytes, and hashes w ith SHA-256
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(hashBytes); // encodes the bytes into base64 and returns as string
     }
 }
