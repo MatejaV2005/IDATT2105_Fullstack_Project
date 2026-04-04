bb8c4fa318833fa85b7685af3a0ddadefd1a49b4
MatejaV2005 <matejanorway@gmail.com>
2026-04-04 17:06:36 +0200

    added repository annotation

--- backend/src/main/java/com/grimni/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/grimni/repository/RefreshTokenRepository.java
@@ -3,9 +3,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.grimni.domain.RefreshToken;
 import com.grimni.domain.User;
 
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
     
     Optional<RefreshToken> findByTokenValue(String tokenValue);
