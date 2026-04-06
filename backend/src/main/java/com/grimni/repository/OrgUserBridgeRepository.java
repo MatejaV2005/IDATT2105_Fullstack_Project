cf7b0a055b85b3a3ecf59b8dc39d00cfc4872460
MatejaV2005 <matejanorway@gmail.com>
2026-04-07 00:07:13 +0200

    add custom query methods in OrgUserBridge repository

--- backend/src/main/java/com/grimni/repository/OrgUserBridgeRepository.java
+++ backend/src/main/java/com/grimni/repository/OrgUserBridgeRepository.java
@@ -1,10 +1,14 @@
 package com.grimni.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.grimni.domain.OrgUserBridge;
 import com.grimni.domain.ids.OrgUserBridgeId;
 
 public interface OrgUserBridgeRepository extends JpaRepository<OrgUserBridge, OrgUserBridgeId> {
-
+    List<OrgUserBridge> findByUserId(Long userId);
+    Optional<OrgUserBridge> findByOrganizationIdAndUserId(Long orgId, Long userId);
 }
