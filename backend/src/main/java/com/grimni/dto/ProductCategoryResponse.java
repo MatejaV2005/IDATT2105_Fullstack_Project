8ee9eb4656805f9de6957e287b3a9265b171a219
MatejaV2005 <matejanorway@gmail.com>
2026-04-10 00:35:40 +0200

    added ProductCategory dto + ProductCategory service class

---
+++ backend/src/main/java/com/grimni/dto/ProductCategoryResponse.java
@@ -0,0 +1,4 @@
+package com.grimni.dto;
+
+public record ProductCategoryResponse(Long id, String productDescription) {
+}
--- backend/src/main/java/com/grimni/repository/ProductCategoryRepository.java
+++ backend/src/main/java/com/grimni/repository/ProductCategoryRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.grimni.domain.ProductCategory;
 
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+    List<ProductCategory> findByOrganization_Id(Long orgId);
     Optional<ProductCategory> findByIdAndOrganization_Id(Long id, Long orgId);
     List<ProductCategory> findByOrganization_IdAndIdIn(Long orgId, Collection<Long> ids);
 }
